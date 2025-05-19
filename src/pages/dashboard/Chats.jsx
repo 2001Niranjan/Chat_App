23f93a9671a531f4eae4ce4592a5c91a89910602
Niranjan Singh <ramsinghvandevi@gmail.com>
2025-05-19 23:25:53 +0530

    feat: initialized chat application

---
+++ src/pages/dashboard/Chats.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+const Chats = () => {
+  return (
+    <Box
+    //   sx={{
+    //     position: "relative",
+    //     height: "100%",
+    //     width: 320,
+    //     backgroundColor: "#F8FAFF",
+    //     boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+    //   }}
+    >
+        hello
+    </Box>
+  );
+};
+
+export default Chats;
